e8d12da9be65b48b8842e4a32d711687ae623b7f
Deepak Patel <deepak.patel.23082005@gmail.com>
2025-02-27 19:52:55 +0530

    Update SymmetricTree.java

--- trees/SymmetricTree.java
+++ trees/SymmetricTree.java
@@ -1,4 +1,18 @@
 package trees;
 
 public class SymmetricTree {
+  public boolean isMirror(TreeNode p,TreeNode q){
+        if(p==null && q==null)return true;
+        if(p==null || q==null)return false;
+
+        return (p.val==q.val)&& isMirror(p.left,q.right)&& isMirror(p.right,q.left);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        if(root==null)return false;
+
+        return isMirror(root.left,root.right);
+    }
+    public static void main(String[] args) {
+
+    }
 }
