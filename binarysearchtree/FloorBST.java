9285983382bc31783ee40741155f521739d08eba
Deepak-patel-here <deepak.patel.23082005@gmail.com>
2025-03-10 14:51:25 +0530

    Solved

---
+++ binarysearchtree/FloorBST.java
@@ -0,0 +1,20 @@
+package binarysearchtree;
+
+public class FloorBST {
+    public static int floor(Node root, int x) {
+        int floor=-1;
+        while(root!=null){
+            if(root.data==x)return root.data;
+            if(root.data>x){
+                root=root.left;
+            }else{
+                floor=root.data;
+                root=root.right;
+            }
+        }
+        return floor;
+    }
+    public static void main(String[] args) {
+
+    }
+}
