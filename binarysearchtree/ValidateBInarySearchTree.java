97b15c51bfe71fcf0e0302eb263b0588f6a93846
Deepak-patel-here <deepak.patel.23082005@gmail.com>
2025-03-12 22:27:59 +0530

    solved

---
+++ binarysearchtree/ValidateBInarySearchTree.java
@@ -0,0 +1,16 @@
+package binarysearchtree;
+
+public class ValidateBInarySearchTree {
+    public static boolean isValidBST(TreeNode root) {
+        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    public boolean helper(TreeNode root,long min,long max){
+        if(root==null)return true;
+        if(root.val<=min || root.val>=max)return false;
+
+        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+    }
+    public static void main(String[] args) {
+
+    }
+}
